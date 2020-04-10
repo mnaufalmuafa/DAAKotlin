9c1d22348bf7b8e1f5432e2f5e113e4098ae42c8
mnaufalmuafa <naufalmuafa77@gmail.com>
2020-04-10 07:24:28 +0700

    Menyelesaikan bubble sort

--- src/bubble.kt
+++ src/bubble.kt
@@ -1,7 +1,20 @@
-fun bubbleSort() {
-    print("Ter")
+fun bubbleSort(arr : Array<Double>) {
+    val arrSize = arr.size
+    for (pass in 0 .. arrSize-2) {
+        for (i in 0 .. arrSize-pass-2) {
+            if (arr[i] > arr[i+1]) {
+                val temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+            }
+        }
+    }
+    arr.forEach {
+        println(it)
+    }
 }
 
 fun main() {
-    bubbleSort()
+    val arr = arrayOf(4.0, 9.5, 7.8, 5.4, 8.9)
+    bubbleSort(arr)
 }
